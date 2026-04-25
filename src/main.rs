1dae3f7c232c7b1d7466e443b746076dd06c0ec3
Sonia Boutaleb <soniaboutaleb@MacBook-Air-de-Sonia.local>
2026-04-25 16:38:48 +0200

    feat(main): handle input file as argument

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+use std::env;
 use std::fs;
+use std::path::Path;
 
 use rust_gestionnaire_telephonique::models::Contact;
 use rust_gestionnaire_telephonique::plantuml::generate_plantuml;
 use rust_gestionnaire_telephonique::trie::Trie;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input_path = "data/01_simple.json";
-    let output_path = "graph/01_simple.puml";
+    let input_path = env::args()
+        .nth(1)
+        .ok_or("Usage: cargo run -- <input_json_path>")?;
 
-    let content = fs::read_to_string(input_path)?;
+    let output_path = build_output_path(&input_path)?;
+
+    let content = fs::read_to_string(&input_path)?;
     let content = content.replace(",\r\n]", "\r\n]");
     let content = content.replace(",\n]", "\n]");
 
@@ -22,9 +27,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let plantuml = generate_plantuml(&trie);
 
-    fs::write(output_path, plantuml)?;
+    fs::write(&output_path, plantuml)?;
 
     println!("Fichier genere : {}", output_path);
 
     Ok(())
+}
+
+fn build_output_path(input_path: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let path = Path::new(input_path);
+
+    let file_stem = path
+        .file_stem()
+        .and_then(|stem| stem.to_str())
+        .ok_or("Impossible de recuperer le nom du fichier d'entree")?;
+
+    Ok(format!("graph/{file_stem}.puml"))
 }
