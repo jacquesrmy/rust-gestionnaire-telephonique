19d0fb089a9bf1d0059751828f5b89ddd923efcb
Sonia Boutaleb <soniaboutaleb@MacBook-Air-de-Sonia.local>
2026-04-26 15:26:21 +0200

    test(trie): add basic unit tests for insert function

--- src/trie.rs
+++ src/trie.rs
@@ -49,3 +49,37 @@ impl Default for Trie {
         Self::new()
     }
 }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_insert_simple() {
+        let mut trie = Trie::new();
+
+        trie.insert("123", "Alice");
+
+        let node_1 = trie.root.children.get(&'1').unwrap();
+        let node_2 = node_1.children.get(&'2').unwrap();
+        let node_3 = node_2.children.get(&'3').unwrap();
+
+        assert_eq!(node_3.name, Some("Alice".to_string()));
+    }
+}
+
+#[test]
+fn test_insert_common_prefix() {
+    let mut trie = Trie::new();
+
+    trie.insert("123", "Alice");
+    trie.insert("124", "Bob");
+
+    let node_1 = trie.root.children.get(&'1').unwrap();
+    let node_2 = node_1.children.get(&'2').unwrap();
+
+    let node_3 = node_2.children.get(&'3').unwrap();
+    let node_4 = node_2.children.get(&'4').unwrap();
+
+    assert_eq!(node_3.name, Some("Alice".to_string()));
+    assert_eq!(node_4.name, Some("Bob".to_string()));
+}
